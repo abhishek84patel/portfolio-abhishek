51cff937d5921ecdaf4c84bcd3d22e844d8038f5
abhishek84patel <mrabhishekpatel24@gmail.com>
2023-07-25 21:35:53 +0530

    all file upload again

---
+++ index.js
@@ -0,0 +1,20 @@
+const menu = document.getElementById("nav");
+const m_icon = document.getElementById("m-img");
+
+// it is called normal and anynomus function
+function showMenu() {
+    const ar = arguments[0]
+    ar.classList.toggle("fixed")
+    console.log()
+    menu.classList.toggle("show");
+
+
+
+}
+// its a arrow function
+let downloadfile = () => {
+    let f = document.getElementById("file").click();
+    // console.log(f)
+    // alert(f)
+
+};
