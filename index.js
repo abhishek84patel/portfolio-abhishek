307bb869dba0f526abe502bfa29ff68f5e909729
Abhishek Patel <108073387+abhishek84patel@users.noreply.github.com>
2023-08-21 10:40:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const menu = document.getElementById("nav");
-const m_icon = document.getElementById("m-img");
+const menu = document.querySelector(".nav");
+// const m_icon = document.getElementById("m-img");
 
 // it is called normal and anynomus function
 function showMenu() {
